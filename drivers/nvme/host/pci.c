0f6678759dff14f19c4ba82118bc2dd83a9fac96
filipecbmoc <31161577+filipecbmoc@users.noreply.github.com>
2021-06-02 11:44:39 -0300

    Update pci.c
    Added QUIRK to ADATA XPG Gammix S70

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3258,6 +3258,8 @@ static const struct pci_device_id nvme_id_table[] = {
 		.driver_data = NVME_QUIRK_LIGHTNVM, },
 	{ PCI_DEVICE(0x10ec, 0x5762),   /* ADATA SX6000LNP */
 		.driver_data = NVME_QUIRK_IGNORE_DEV_SUBNQN, },
+        { PCI_DEVICE(0x1dbe, 0x5236),   /* ADATA XPG GAMMIX S70 */
+                .driver_data = NVME_QUIRK_IGNORE_DEV_SUBNQN, },
 	{ PCI_DEVICE(0x1cc1, 0x8201),   /* ADATA SX8200PNP 512GB */
 		.driver_data = NVME_QUIRK_NO_DEEPEST_PS |
 				NVME_QUIRK_IGNORE_DEV_SUBNQN, },
